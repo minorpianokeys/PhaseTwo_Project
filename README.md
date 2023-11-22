8a1149c5e1860e29220f9620e76c7d055ccaf0d4
Michael Friedman <minorpianokeys@gmail.com>
2023-11-22 13:57:15 -0500

    added server script

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cocktail Atlas
+
+Welcome to Cocktail Atlas, your go-to resource for discovering and exploring a variety of delicious cocktails! Whether you're a seasoned mixologist or a casual drink enthusiast, this website provides a curated collection of cocktail recipes to suit every taste.
+
+## Features
+
+- **Browse Cocktails:** Explore a diverse selection of cocktails, each with detailed recipes and instructions.
+
+- **Search Functionality:** Easily find your favorite cocktails or discover new ones by using the search feature.
+
+- **Filter by Spirit:** Looking for a specific spirit? Use the Spirit filter to narrow down your options.
--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -5,7 +5,7 @@ import "../styles/Directory.css"
 
 function Directory() {
     const [cocktails, setCocktails] = useState([]);
-    const [filteredCocktails, setFilteredCocktails] = useState([]);
+    // const [filteredCocktails, setFilteredCocktails] = useState([]);
     const [searchInput, setSearchInput] = useState("");
     const [filter, setFilter] = useState("All");
 
@@ -15,16 +15,19 @@ function Directory() {
       .then(cocktailData => setCocktails(cocktailData))
     }, [])
 
-    useEffect(() => {
-        const filtered = cocktails.filter((cocktail) => {
-          return (
-            cocktail.name.toLowerCase().includes(searchInput.toLowerCase()) &&
-            (filter === "All" || cocktail.spirit === filter)
-          );
-        });
+    // useEffect(() => {
+    //     const filtered = cocktails.filter((cocktail) => {
+    //       return (
+    //         cocktail.name.toLowerCase().includes(searchInput.toLowerCase()) &&
+    //         (filter === "All" || cocktail.spirit === filter)
+    //       );
+    //     });
     
-        setFilteredCocktails(filtered);
-      }, [cocktails, searchInput, filter]);
+    //     setFilteredCocktails(filtered);
+    //   }, [cocktails, searchInput, filter]);
+
+    const filteredCocktails = cocktails
+    .filter(cocktail => cocktail.name.toLowerCase().includes(searchInput.toLowerCase()))
 
     return (
         <div>
